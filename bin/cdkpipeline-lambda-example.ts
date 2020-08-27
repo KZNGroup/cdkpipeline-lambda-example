784d2abbf9f11ed271eb1a433c520618ce66e11a
Gareth Evans <evans.g.p@gmail.com>
2020-08-27 13:42:45 +0800

    add self-mutating pipeline

--- bin/cdkpipeline-lambda-example.ts
+++ bin/cdkpipeline-lambda-example.ts
@@ -2,6 +2,12 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { CdkpipelineLambdaExampleStack } from '../lib/cdkpipeline-lambda-example-stack';
+import { CdkpipelineLambdaExamplePipeline } from '../lib/cdkpipeline-lambda-example-pipeline';
 
 const app = new cdk.App();
+
 new CdkpipelineLambdaExampleStack(app, 'CdkpipelineLambdaExampleStack');
+
+new CdkpipelineLambdaExamplePipeline(app, 'cdkpipeline-lambda-example-pipeline');
+
+app.synth();
---
+++ lib/cdkpipeline-lambda-example-pipeline.ts
@@ -0,0 +1,34 @@
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+import * as cdk from '@aws-cdk/core';
+import * as pipelines from "@aws-cdk/pipelines";
+
+export class CdkpipelineLambdaExamplePipeline extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const sourceArtifact = new codepipeline.Artifact();
+    const cloudAssemblyArtifact = new codepipeline.Artifact();
+
+    const pipeline = new pipelines.CdkPipeline(this, 'Pipeline', {
+      pipelineName: 'cdkpipeline-lambda-example',
+      cloudAssemblyArtifact,
+
+      sourceAction: new codepipeline_actions.GitHubSourceAction({
+        actionName: 'GitHub',
+        output: sourceArtifact,
+        oauthToken: cdk.SecretValue.secretsManager('secret-name'),
+        owner: 'KZNGroup',
+        repo: 'cdkpipeline-lambda-example',
+        branch: 'master'
+      }),
+
+      synthAction: pipelines.SimpleSynthAction.standardNpmSynth({
+        sourceArtifact,
+        cloudAssemblyArtifact,
+        buildCommand: 'npm run build'
+      }),
+    });
+
+  }
+}
